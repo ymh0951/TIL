e579e023b5e7e03895190376d8fbed36ab08ed42
Minho Yoo <ymh0951@naver.com>
2022-05-03 18:04:05 +0900

    Node study 🌈

--- node/prime.js
+++ node/prime.js
@@ -2,4 +2,10 @@ const min = 2;
 const max = 10_100_100;
 const primes = [];
 
-function
+function generatePrimes(start, range) {
+    let isPrime = true;
+    const end = start + range;
+    for(let i = start; i < end; i++) {
+        if()
+    }
+}
