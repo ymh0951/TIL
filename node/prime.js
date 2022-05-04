2cc766c042265daed6346bdd117bd45dccd4ef7d
Minho Yoo <ymh0951@naver.com>
2022-05-04 14:32:06 +0900

    Node study 🚀

--- node/prime.js
+++ node/prime.js
@@ -1,11 +1,25 @@
 const min = 2;
-const max = 10_100_100;
+const max = 10_000_000;
 const primes = [];
 
 function generatePrimes(start, range) {
     let isPrime = true;
     const end = start + range;
     for(let i = start; i < end; i++) {
-        if()
+        for(let j = min; j < Math.sqrt(end); j++) {
+            if(i !== j && i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) {
+            primes.push(i);
+        }
+        isPrime = true;
     }
 }
+
+console.time('prime');
+generatePrimes(min, max);
+console.timeEnd('prime');
+console.log(primes.length);
