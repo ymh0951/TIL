cc4400555f230f97cbc2a493652b4fa8cda290b3
Minho Yoo <ymh0951@naver.com>
2022-05-04 18:00:58 +0900

    Node study 🚀

--- node/writeFile.js
+++ node/writeFile.js
@@ -1,8 +1,10 @@
 const fs = require('fs').promises;
 
-fs.readFile('./readme.txt')
+fs.writeFile('./writeme.txt', '글이 입력됩니다.')
+    .then(() => {
+        return fs.readFile('./writeme.txt');
+    })
     .then((data) => {
-        console.log(data);
         console.log(data.toString());
     })
     .catch((err) => {
