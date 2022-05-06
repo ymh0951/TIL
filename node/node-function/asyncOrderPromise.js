bfec637d991360b91bef79c6c718ae4471699294
Minho Yoo <ymh0951@naver.com>
2022-05-06 10:21:32 +0900

    Node study 🚀

---
+++ node/node-function/asyncOrderPromise.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+
+async function main() {
+    let data = await fs.readFile('./readme.txt');
+    console.log('1번', data.toString());
+    data = await fs.readFile('./readme.txt');
+    console.log('2번', data.toString());
+    data = await fs.readFile('./readme.txt');
+    console.log('3번', data.toString());
+    data = await fs.readFile('./readme.txt');
+    console.log('4번', data.toString());
+}
+main();
