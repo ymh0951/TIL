fecb0ce6e7ce896e44e5bb339e52ddc409e19db9
Minho Yoo <ymh0951@naver.com>
2022-05-08 21:32:33 +0900

    node study

--- node/express/app.js
+++ node/express/app.js
@@ -1,62 +1,45 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const multer = require('multer');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 
 const app = express();
-
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
-app.use(cookieParser('zerochopassword'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'zerochopasword',
-    cookie: {
-        httpOnly: true,
-    }
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'session-cookie',
 }));
 
-app.use((req, res, next) => {
-    if(req.session.id) {
-        express.static(path.join(__dirname, 'public'))(req, res, next);
-    } else {
-        next();
-    }
-});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res, next) => {
-    req.session
-    res.sendFile(path.join(__dirname, './index.html'));
-});
-
-app.post('/', (req, res) => {
-    res.send('hello express');
-});
-
-app.get('/category/:name', (req, res) => {
-    res.send(`hello wildcard`);
-});
-
-app.get('/about', (req, res) => {
-    res.send('hello express');
-});
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('404지롱');
+  res.status(404).send('Not Found');
 });
 
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('에러났지롱. 근데 안알려주지롱')
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
-    console.log('익스프레스 서버 실행');
+  console.log(app.get('port'), '번 포트에서 대기 중');
 });
