8d35aef054454a4ff1add01f0bf52b609a96b1cb
Minho Yoo <ymh0951@naver.com>
2022-05-16 17:57:41 +0900

    create table 🚀

---
+++ node/node-bird/models/post.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Post extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            content: {
+                type: Sequelize.STRING(140),
+                allowNull: false,
+            },
+            img: {
+                type: Sequelize.STRING(200),
+                allowNull: true,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'post',
+            tableName: 'posts',
+            paranoid: false,
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+        });
+    }
+
+    static associate(db) {
+        db.Post.belongsTo(db.User);
+        db.Post.belongToMany(db.Hashtag, { through: 'PostHashtag' });
+    }
+};
