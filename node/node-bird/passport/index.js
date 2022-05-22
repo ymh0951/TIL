0e2b38d48f0bbcb6a53e498a11ff584dd626af0a
Minho Yoo <ymh0951@naver.com>
2022-05-22 23:38:04 +0900

    Node study 🚀

--- node/node-bird/passport/index.js
+++ node/node-bird/passport/index.js
@@ -4,27 +4,27 @@ const kakao = require('./kakaoStrategy');
 const User = require('../models/user');
 
 module.exports = () => {
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-    passport.deserializeUser((id, done) => {
-        User.findOne({
-             where: { id },
-             include: [{
-                 model: User,
-                 attributes: ['id', 'nick'],
-                 as: 'Followers',
-             }, {
-                 model: User,
-                 attributes: ['id', 'nick'],
-                 as: 'Followings',
-             }],
-            })
-            .then(user => done(null, user))
-            .catch(err => done(err));
-        });
-        
-    local();
-    kakao();
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
+      }],
+    })
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+
+  local();
+  kakao();
 };
--- node/node-bird/passport/kakaoStrategy.js
+++ node/node-bird/passport/kakaoStrategy.js
@@ -4,29 +4,29 @@ const KakaoStrategy = require('passport-kakao').Strategy;
 const User = require('../models/user');
 
 module.exports = () => {
-    passport.use(new KakaoStrategy({
-        clientID: process.env.KAKAO_ID,
-        callbackURL: '/auth/kakao/callback',
-    }, async (accessToken, refreshToken, profile, done) => {
-        console.log('kakao profile', profile);
-        try {
-            const exUser = await User.findOne({
-                where: { snsId: profile.id, provider: 'kakao' },
-            });
-            if (exUser) {
-                done(null, exUser);
-            } else {
-                const newUser = await User.create({
-                    email: profile._json && profile._json.kakao_account_email,
-                    nick: profile.displayName,
-                    snsId: profile.id,
-                    provider: 'kakao',
-                });
-                done(null, newUser);
-            }
-        } catch (error) {
-            console.error(error);
-            done(error);
-        }
-    }));
-}
+  passport.use(new KakaoStrategy({
+    clientID: process.env.KAKAO_ID,
+    callbackURL: '/auth/kakao/callback',
+  }, async (accessToken, refreshToken, profile, done) => {
+    console.log('kakao profile', profile);
+    try {
+      const exUser = await User.findOne({
+        where: { snsId: profile.id, provider: 'kakao' },
+      });
+      if (exUser) {
+        done(null, exUser);
+      } else {
+        const newUser = await User.create({
+          email: profile._json && profile._json.kakao_account_email,
+          nick: profile.displayName,
+          snsId: profile.id,
+          provider: 'kakao',
+        });
+        done(null, newUser);
+      }
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
+  }));
+};
