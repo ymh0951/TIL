352b8e847acd883e106d73c332d78910952e7313
Minho Yoo <ymh0951@naver.com>
2022-07-01 16:06:07 +0900

    Study TypeScript + Vue 🚀

--- typescript/typescript-in-vue/vue-todo/src/App.vue
+++ typescript/typescript-in-vue/vue-todo/src/App.vue
@@ -1,29 +1,110 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <header>
+      <h1>Vue Todo with Typescript</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoItem"
+      ></TodoInput>
+      <div>
+        <ul>
+          <TodoListItem
+            v-for="(todoItem, index) in todoItems"
+            :key="index"
+            :index="index"
+            :todoItem="todoItem"
+            @toggle="toggleTodoItemComplete"
+            @remove="removeTodoItem"
+          ></TodoListItem>
+          <!-- <li>아이템 1</li>
+          <li>아이템 2</li>
+          <li>아이템 3</li> -->
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: Todo[]) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch(): Todo[] {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
+
+export interface Todo {
+  title: string;
+  done: boolean;
+}
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { TodoInput, TodoListItem },
+  data() {
+    return {
+      todoText: "",
+      todoItems: [] as Todo[],
+    };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      const todo: Todo = {
+        title: value,
+        done: false,
+      };
+      this.todoItems.push(todo);
+      storage.save(this.todoItems);
+      // localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = "";
+    },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch().sort((a, b) => {
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title > b.title) {
+          return 1;
+        }
+        return 0;
+      });
+    },
+    removeTodoItem(index: number) {
+      console.log("remove", index);
+      this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
+    },
+    toggleTodoItemComplete(todoItem: Todo, index: number) {
+      this.todoItems.splice(index, 1, {
+        ...todoItem,
+        done: !todoItem.done,
+      });
+      storage.save(this.todoItems);
+    },
+  },
+  created() {
+    this.fetchTodoItems();
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
--- typescript/typescript-in-vue/vue-todo/src/main.ts
+++ typescript/typescript-in-vue/vue-todo/src/main.ts
@@ -6,3 +6,11 @@ Vue.config.productionTip = false;
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
+
+/**
+ * 할 일 관리 앱 (CRUD)
+ * - [x] 할 일 조회
+ * - [x] 할 일 추가
+ * - [x] 할 일 삭제
+ * - [x] 할 일 수정
+ */
