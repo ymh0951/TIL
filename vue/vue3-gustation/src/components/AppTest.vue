414542c61b8820feac6e49f57e40be76db346467
Minho Yoo <ymh0951@naver.com>
2022-09-19 18:01:12 +0900

    vue3 맛보기

---
+++ vue/vue3-gustation/src/components/AppTest.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <h2>{{ $store.state.storeValue }}</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    },
+    data() {
+        return {
+            message: 'Hello World, Hi Vue3!' as string,
+        }
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/vue3-gustation/src/store/index.ts
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    storeValue: 'Vuex에서 가져왔습니다.' as string,
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
