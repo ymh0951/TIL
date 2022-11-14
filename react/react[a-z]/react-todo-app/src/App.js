47aa4bbc96967e00a8859db5b21d9ede5946da43
Minho Yoo <ymh0951@naver.com>
2022-11-14 20:38:14 +0900

    tailwind css 적용

--- react/react[a-z]/react-todo-app/src/App.js
+++ react/react[a-z]/react-todo-app/src/App.js
@@ -26,6 +26,7 @@ export default function App() {
         <div className='title'>
           <h1>할 일 목록</h1>
         </div>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
         <List todoData={todoData} setTodoData={setTodoData} />
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
