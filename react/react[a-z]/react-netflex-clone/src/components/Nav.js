c1c59f020bb1ad9047008ab067dfce2f5428001e
Minho Yoo <ymh0951@naver.com>
2022-11-17 16:41:53 +0900

    search 추가

--- react/react[a-z]/react-netflex-clone/src/components/Nav.js
+++ react/react[a-z]/react-netflex-clone/src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './Nav.css'
 
 export default function Nav() {
     const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+    const navigate = useNavigate();
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -18,6 +21,11 @@ export default function Nav() {
         };
     }, []);
 
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+        navigate(`/search?q=${e.target.value}`);
+    }
+
     return (
         <nav className={`nav ${show && 'nav__black'}`}>
             <img
@@ -25,6 +33,13 @@ export default function Nav() {
                 src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png'
                 className='nav__logo'
                 onClick={() => window.location.reload()}
+            />
+            <input 
+                value={searchValue}
+                onChange={handleChange}
+                className='nav__input'
+                type='text'
+                placeholder='영화를 검색해주세요.'
             />
             <img 
                 alt='User logged'
