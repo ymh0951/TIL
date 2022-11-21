1d1a0907cf84146444e59a779e0d54fb20b07fc2
Minho Yoo <ymh0951@naver.com>
2022-11-21 17:46:11 +0900

    ㅇㅇ

--- react/react[a-z]/react-netflex-clone/src/api/axios.js
+++ react/react[a-z]/react-netflex-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
-        api_key: 'f1e7da3f22c0c8a2f0ad3287d5c15e0d',
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: 'ko-KR'
     },
 });
