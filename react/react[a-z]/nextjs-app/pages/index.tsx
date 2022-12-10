c820c6aa4793d0f86db17d46a97e4a67cd86904f
Minho Yoo <ymh0951@naver.com>
2022-12-10 19:46:29 +0900

    Study Next.js

--- react/react[a-z]/nextjs-app/pages/index.tsx
+++ react/react[a-z]/nextjs-app/pages/index.tsx
@@ -15,7 +15,7 @@ const Home = ({
   }[];
 }) => {
   return (
-    <div>
+    <div className={homeStyles.container}>
       <Head>
         <title>Minho Yoo</title>
       </Head>
--- react/react[a-z]/nextjs-app/pages/posts/[id].tsx
+++ react/react[a-z]/nextjs-app/pages/posts/[id].tsx
@@ -1,11 +1,28 @@
-import { GetStaticPaths } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import React from "react";
-import { getAllPostIds } from "../../lib/post";
+import { getAllPostIds, getPostData } from "../../lib/post";
+import postStyle from '../../styles/Post.module.css';
 
-const Post = () => {
+const Post = ({ postData }: {
+    postData: {
+      title: string,
+      date: string,
+      contentHtml: string
+    }
+} ) => {
   return (
-    <div>
-        안녕
+    <div className={postStyle.container}>
+        <Head>
+          <title>{ postData.title }</title>
+        </Head>
+        <article>
+          <h1>{postData.title}</h1>
+          <div>
+            {postData.date}
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
     </div>
   )
 };
@@ -19,4 +36,15 @@ export const getStaticPaths: GetStaticPaths = async () => {
         paths,
         fallback: false
     }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+
+  return {
+    props: {
+      postData
+    }
+  }
+
 }
